cd9f0461b81431703d93e40a6b3265ec8115d500
NguyenBui221 <Shiyigebuweiii@gmail.com>
2024-05-12 22:24:32 +0700

    Web

--- js/script.js
+++ js/script.js
@@ -53,3 +53,13 @@ menu.addEventListener('click',()=>{
 menuClose.addEventListener('click',()=>{
     document.querySelector('.header-menu-movil').classList.remove('active');
 })
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
